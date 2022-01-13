bc91692f2c69e3c5fa7e63ce45013d0a49701e4e
Herschel Launch <hlaunch@mi-p3.msk.21-school.ru>
2022-01-13 04:27:22 +0300

    dsjhbdf

---
+++ pipex.h
@@ -0,0 +1,12 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include "libft/libft.h"
+
+#endif
---
+++ test.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlaunch <hlaunch@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/11 01:28:55 by hlaunch           #+#    #+#             */
+/*   Updated: 2022/01/12 21:48:31 by hlaunch          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+/*int	parse_cmd(char *cmd, const char **env)
+{
+	int		i;
+	char	*path;
+
+	i = 0;
+	while (*env[i])
+	{
+		if (ft_strncmp("PATH=", env[i], 6) == 0)
+			return (env[i]);
+		
+	}
+}*/
+
+int main(int argc, char **argv, const char **env)
+{
+	pid_t	cmd1;
+	pid_t	cmd2;
+	int		fifo[2];
+	int		fd;
+	char	*arg[4];
+
+	if (argc != 5)
+		return (-1);
+	arg[0] = "sh";
+	arg[1] = "-c";
+	arg[2] = argv[2];
+	arg[3] = 0;
+	if (pipe(fifo) == -1)
+	{
+		perror("pipe");
+		exit(1);
+	}
+	cmd1 = fork();
+	if (!cmd1)
+	{
+		fd = open(argv[1], O_RDONLY);
+		dup2(fd, STDIN_FILENO);
+		dup2(fifo[0], STDOUT_FILENO);
+		close(fifo[0]);
+		close(fifo[1]);
+		execve("/bin/sh", arg,(char *const *) env);
+	}
+	cmd2 = fork();
+	if (!cmd2)
+	{
+		fd = open(argv[4], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+		dup2(fifo[0], STDIN_FILENO);
+		dup2();
+		close(fifo[0]);
+		close(fifo[1]);
+		execve("/bin/sh", arg, (char *const *) env);
+	}
+	close(fifo[0]);
+	close(fifo[1]);
+	while (wait(&cmd1) != cmd1 || wait(&cmd2) != cmd2)
+		return (1);
+	return (0);
+}
